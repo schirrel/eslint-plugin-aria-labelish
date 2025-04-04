c0e240c99b4b444724613643a1986b3c9f68801b
Alan Schio <6757777+schirrel@users.noreply.github.com>
2025-04-04 13:47:44 -0400

    Init

---
+++ index.js
@@ -0,0 +1,6 @@
+module.exports = {
+    rules: {
+        'aria-label-not-allowed': require('./rules/aria-label-not-allowed'),
+        'aria-labelledby-not-allowed': require('./rules/aria-labelledby-not-allowed')
+    }
+};
